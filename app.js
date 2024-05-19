94da859ccd3abcd437539bf5c551f5aa1eabaf4e
Alexandre Machado <117436159+LXMachado@users.noreply.github.com>
2024-05-19 19:04:20 +1000

    Updated contact section with Calendly button and replaced WordPress Development with Framer Development

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*==================== toggle icon navbar ====================*/
+/*==================== Toggle icon navbar ====================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,8 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
-/*==================== scroll sections active link ====================*/
+/*==================== Scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -19,25 +18,23 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        };
+        }
     });
-    /*==================== sticky navbar =========================*/
+    /*==================== Sticky navbar ====================*/
     let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-    /*header.classList.toggle('sticky', window.scrollY > 100);
-
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    /*==================== Remove toggle icon and navbar (scroll) ====================*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
-
-/*==================== scroll reveal ====================*/
+/*==================== Scroll reveal ====================*/
 ScrollReveal({
     // reset: true,
     distance: '80px',
@@ -50,10 +47,9 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact 
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-
-/*==================== typed js ====================*/
+/*==================== Typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['HTML', 'CSS', 'Bootstrap', 'Tailwind','JavaScript', 'React.JS', 'Redux', 'Node.JS', 'Express', 'Wordpress', 'PHP', 'Postgre SQL', '', 'ISTQB', 'Agile PM', 'Scrum Master'],
+    strings: ['HTML', 'CSS', 'Bootstrap', 'Tailwind', 'JavaScript', 'React', 'Redux', 'Node.js', 'Express', 'WordPress', 'PHP', 'PostgreSQL', 'ISTQB', 'Agile PM', 'Scrum Master'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
